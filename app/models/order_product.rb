c4dea8ce8b2a47669381e10b02d19254bbcabf51
jl <jaeli_ottawa@outlook.com>
2025-03-01 08:41:13 -0500

    feat: add order_product validation and rspec tests

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,8 @@
 class OrderProduct < ApplicationRecord
+  validates :product_id, presence: true
+  validates :order_id, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+
   belongs_to :product
   belongs_to :order
 end
---
+++ spec/models/order_product_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe OrderProduct, type: :model do
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:product_id) }
+    it { is_expected.to validate_presence_of(:order_id) }
+    it { is_expected.to validate_presence_of(:quantity) }
+    it { is_expected.to validate_numericality_of(:quantity).is_greater_than(0) }
+  end
+
+  context 'associations' do
+    it { is_expected.to belong_to(:product) }
+    it { is_expected.to belong_to(:order) }
+  end
+end
+    
