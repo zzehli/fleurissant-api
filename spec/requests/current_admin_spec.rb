100999ad5b2fa42779410db80e447038752ab407
jl <jaeli_ottawa@outlook.com>
2025-03-19 13:06:23 -0400

    feat: add current admins rout test

--- spec/requests/current_admin_spec.rb
+++ spec/requests/current_admin_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "CurrentAdmins", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/current_admin/index"
-      expect(response).to have_http_status(:success)
+    let(:admin) { create(:admin) }
+
+    before do
+      login_user(admin)
+      puts JSON.parse(response.body)
+      get "/current_admin/", headers: {
+        Authorization: response.headers['Authorization']
+      }
+    end
+
+    after do |example|
+      if example.exception
+        Rails.logger.error "Response body: #{response.body}"
+      end
     end
   end
 end
