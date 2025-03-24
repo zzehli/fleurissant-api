e23743afb898384434e8202799c84f52764b191d
jl <jaeli_ottawa@outlook.com>
2025-03-24 12:52:06 -0400

    BREAKING CHANGE: switch db to sqlite

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem "rails", "~> 7.2.2"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 # Use pg as the database for Active Record
-gem "pg", "~> 1.5"
+# gem "pg", "~> 1.5"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
@@ -15,7 +15,7 @@ gem "turbo-rails"
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
 # Use Tailwind CSS [https://github.com/rails/tailwindcss-rails]
-gem "tailwindcss-rails"
+gem "tailwindcss-rails", "~> 3.3.1"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
@@ -75,3 +75,5 @@ gem "stripe", "~> 13.3"
 gem "rack-cors"
 
 gem "jsonapi-serializer"
+
+gem "sqlite3"
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_03_19_151922) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -64,8 +61,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_19_151922) do
   end
 
   create_table "order_products", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "order_id", null: false
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -93,7 +90,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_19_151922) do
   end
 
   create_table "stocks", force: :cascade do |t|
-    t.bigint "product_id", null: false
+    t.integer "product_id", null: false
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
