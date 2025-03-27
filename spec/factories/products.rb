469d01a79255359e689b8e9046679640e8e494ca
jl <jaeli_ottawa@outlook.com>
2025-03-27 16:14:48 -0400

    fix: add img to product tests

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
     factory :product do
         name { Faker::Commerce.product_name }
         sequence(:price) { |n| n }
+        image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'files', 'test.avif'), 'image/avif') }
     end
 end
