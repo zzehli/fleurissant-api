346b64a8470330bfaad2db43814d75427326d288
jl <jaeli_ottawa@outlook.com>
2025-03-30 10:01:21 -0400

    add correct frontend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-    origins Rails.env.production? ? 'https://fleurissant.jaeli.my.id' : 'http://localhost:5173' # Use different origins for production and development
+    origins Rails.env.production? ? 'https://zzehli.github.io/fleurissant/' : 'http://localhost:5173' # Use different origins for production and development
       resource '*',
         headers: :any,
         expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
