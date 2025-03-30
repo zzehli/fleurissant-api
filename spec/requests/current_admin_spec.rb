80833f3caed430401e3e62e5cfcc323228c6e9c8
jl <jaeli_ottawa@outlook.com>
2025-03-30 18:22:45 -0400

    feat: add current admin test

--- spec/requests/current_admin_spec.rb
+++ spec/requests/current_admin_spec.rb
@@ -6,10 +6,19 @@ RSpec.describe "CurrentAdmins", type: :request do
 
     before do
       login_user(admin)
-      puts JSON.parse(response.body)
+      # puts JSON.parse(response.body)
       get "/current_admin/", headers: {
         Authorization: response.headers['Authorization']
       }
+    end
+
+    it 'returns a 200 OK status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns the current admin' do
+      json = JSON.parse(response.body)
+      expect(json['email']).to eq(admin.email)
     end
 
     after do |example|
