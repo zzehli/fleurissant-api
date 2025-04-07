0986b59407c5d0922948417ec52e6c146acdcfe6
jl <jaeli_ottawa@outlook.com>
2025-04-07 15:23:07 -0400

    fix: pagination not set correctly

--- app/models/paginator.rb
+++ app/models/paginator.rb
@@ -5,10 +5,10 @@ module Paginator
     @content_per_page = num
   end
   # return desired page content calculated based on page number and content per page
-  def page(page_number, per_page = @content_per_page)
-    page_num ||= 1
-    per_page ||= @content_per_page
+  def page(page, limit = @content_per_page)
+    page ||= 1
+    limit ||= @content_per_page
 
-    offset((page_num - 1) * per_page).limit(per_page)
+    offset((page - 1) * limit).limit(limit)
   end
 end
