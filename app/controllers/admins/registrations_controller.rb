d0c2a7257cf518942224dc3c566251cd2efe1d15
jl <jaeli_ottawa@outlook.com>
2025-04-08 20:33:16 -0400

    chore: update session msg

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -20,19 +20,4 @@ class Admins::RegistrationsController < Devise::RegistrationsController
       }, status: :unprocessable_entity
     end
   end
-
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
 end
--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -4,30 +4,12 @@ class Admins::SessionsController < Devise::SessionsController
   respond_to :json
   private
   def respond_with(resource, _opts = {})
-    if request.method == "POST" && resource.persisted?
-      render json: {
-        status: { code: 200, message: "Logged in successfully." },
-        data: AdminSerializer.new(resource).serializable_hash[:data][:attributes]
-      }, status: :ok
-    else
-      render json: {
-        status: { code: 422, message: "Admin user couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
-      }, status: :unprocessable_entity
+    if resource.errors.empty?
+      render json: resource
     end
   end
 
   def respond_to_on_destroy
-      if current_admin
-      Rails.logger.debug "Current admin: #{current_admin.inspect}"
-      render json: {
-        status: 200,
-        message: "logged out successfully"
-      }, status: :ok
-      else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
-      end
+    head :no_content
   end
 end
