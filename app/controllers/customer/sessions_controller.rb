7effe6a1ccc6edfcd73035f84f4fd151b8a00964
jl <jaeli_ottawa@outlook.com>
2025-04-23 10:55:09 -0400

    feat: remove get method in customer session controller

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -2,14 +2,18 @@
 
 class Customer::SessionsController < Devise::SessionsController
   respond_to :json
-  private
-  def respond_with(resource, _opts = {})
-    if resource.errors.empty?
-      render json: resource
-    end
+  # skip_before_action only: [ :new ]
+  def new
+    head :not_found
   end
+  # private
+  # def respond_with(resource, _opts = {})
+  #   if resource.errors.empty?
+  #     render json: resource
+  #   end
+  # end
 
-  def respond_to_on_destroy
-    head :no_content
-  end
+  # def respond_to_on_destroy
+  #   head :no_content
+  # end
 end
