14c3f752abbb43db731cd3d4924e796d52129b9d
jl <jaeli_ottawa@outlook.com>
2025-04-24 10:50:29 -0400

    fix: cors not working, remove slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-    origins Rails.env.production? ? 'https://jaeli.space/' : 'http://localhost:5173' # Use different origins for production and development
+    origins Rails.env.production? ? 'https://jaeli.space' : 'http://localhost:5173' # Use different origins for production and development
       resource '*',
         headers: :any,
         expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
